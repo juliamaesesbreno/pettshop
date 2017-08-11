ff9d165a8fa65b086a2e735caffe16405f3a47b5
Julia Sesbreno <juliasesbreno@Ericks-MacBook-Pro.local>
2017-08-11 12:17:53 +0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pettshop::Application.config.secret_token = '5acefd173a22515af6e0aad7e052cf29d784f03bf94c1004ce31edae679626775f48b2aa227c204d22c5375f2fb2f7e713eeef11bb24792200961866583b4f15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pettshop::Application.config.session_store :cookie_store, key: '_pettshop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pettshop::Application.config.session_store :active_record_store
